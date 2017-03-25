3fca2f630e55e1e597eeb045479ca6561a9b6449
lucong <332553882@qq.com>
2017-03-26 03:03:06 +0800

    ad

--- application/controller/admin/Member.php
+++ application/controller/admin/Member.php
@@ -16,11 +16,22 @@ class Member extends Admin
         $order = 'create_time desc';
         $query = [];
         $map  = [];
-        $list = db('member')->where($map)->order($order)->paginate(20 , false , ['query'=>$query]);
+
+        $keyword = input('keyword' , '');
+        if ($keyword){
+            $where = "`name` like '%".$keyword."%' or `phone` like '%".$keyword."%'";
+            $query['keyword'] = $keyword;
+        }else{
+            $where = "";
+        }
+        $this->assign('keyword' , $keyword);
+
+        $list = db('member')->where($map)->where($where)->order($order)->paginate(20 , false , ['query'=>$query]);
         $page = $list->render();
 
         $this->assign('list' , $list);
         $this->assign('page' , $page);
 
+        return $this->fetch();
     }
 }
